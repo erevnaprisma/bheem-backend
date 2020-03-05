a157a14833483155f2ea0f834423b17e17dda5f6
michaelleopold12 <michaelleopold16@gmail.com>
2020-03-05 17:24:20 +0700

    adding validation in saldo services

--- src/collections/saldo/services.js
+++ src/collections/saldo/services.js
@@ -1,6 +1,9 @@
 const Saldo = require('./Model')
 
 const createSaldo = async (userID, finalAmount) => {
+  if (!userID) return { status: 400, error: 'Invalid user id' }
+  else if (!finalAmount) return { status: 400, error: 'Invalid final amount' }
+
   const saldo = new Saldo({
     user_id: userID,
     saldo: finalAmount
@@ -10,6 +13,9 @@ const createSaldo = async (userID, finalAmount) => {
 }
 
 const updateSaldo = async (saldoID, finalAmount) => {
+  if (!saldoID) return { status: 400, error: 'Invalid saldo id' }
+  else if (!finalAmount) return { status: 400, error: 'Invalid final amount' }
+
   await Saldo.updateOne({ saldo_id: saldoID }, { saldo: finalAmount })
 }
 
