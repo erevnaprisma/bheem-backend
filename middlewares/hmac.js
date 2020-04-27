0d3212425530394d6fde685f690745ac6b517354
michaelleopold12 <michaelleopold16@gmail.com>
2020-04-27 14:30:06 +0800

    change key in hmac

--- middlewares/hmac.js
+++ middlewares/hmac.js
@@ -4,7 +4,7 @@ module.exports = function () {
   return function (req, res, next) {
     const { hmac } = req.headers
 
-    const signature = CryptoJs.HmacSHA256(req.body, 'erevna123').toString()
+    const signature = CryptoJs.HmacSHA256(req.body, 'rayapay123#4556*').toString()
 
     if (signature !== hmac) {
       throw new Error('Invalid Hmac')
