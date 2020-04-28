53d64cde4a542bde1556f5175b0e291a4df3d90a
michaelleopold12 <michaelleopold16@gmail.com>
2020-04-28 10:30:41 +0800

    put hmac sha256 to config

--- middlewares/hmac.js
+++ middlewares/hmac.js
@@ -1,10 +1,11 @@
 const CryptoJs = require('crypto-js')
+const config = require('config')
 
 module.exports = function () {
   return function (req, res, next) {
     const { hmac } = req.headers
 
-    const signature = CryptoJs.HmacSHA256(req.body, 'rayapay123#4556*').toString()
+    const signature = CryptoJs.HmacSHA256(req.body, config.get('hmac')).toString()
 
     if (signature !== hmac) {
       throw new Error('Invalid Hmac')
