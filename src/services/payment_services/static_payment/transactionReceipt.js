bb953a34d7aef1e33030f72c793e57bfb523d0cb
michaelleopold12 <michaelleopold16@gmail.com>
2020-05-05 18:52:45 +0800

    add merchant name response to transaction receipt

--- src/services/payment_services/static_payment/transactionReceipt.js
+++ src/services/payment_services/static_payment/transactionReceipt.js
@@ -1,8 +1,15 @@
 const { getTransaction } = require('../../../collections/transaction/services')
 
+const Merchant = require('../../../collections/merchant/Model')
+
 const transactionReceipt = async (transactionID) => {
   try {
     const res = await getTransaction(transactionID)
+
+    const merchant = await Merchant.findOneAndUpdate({ merchant_id: res.merchant_id })
+
+    res.merchant_name = merchant.business_name
+
     return { status: 200, success: 'Successfully get Receipt', transaction: res }
   } catch (err) {
     return { status: 400, error: err || 'Transaction Receipt failed' }
