07acd77686ebcd193233bbc7a61fd763afb4162b
michaelleopold12 <michaelleopold16@gmail.com>
2020-05-05 15:00:24 +0800

    change username to business_name

--- src/services/payment_services/static_payment/detailPayment.js
+++ src/services/payment_services/static_payment/detailPayment.js
@@ -5,10 +5,9 @@ const transactionDetail = async (transactionID) => {
   try {
     const res = await getTransaction(transactionID)
 
-    const merchant = await Merchant.findOne({ merchant_id: res.merchant_id }).select('username -_id')
-    console.log('merchatn username=', merchant)
+    const merchant = await Merchant.findOne({ merchant_id: res.merchant_id })
 
-    res.merchant_name = merchant.username
+    res.merchant_name = merchant.business_name
     return { status: 200, success: 'Successfully get Transaction Detail', transaction: res }
   } catch (err) {
     return { status: 400, error: err || 'Transaction Detail failed' }
