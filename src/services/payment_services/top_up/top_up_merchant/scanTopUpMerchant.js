a316809fe292718f5e980a2814083adf7b86d910
michaelleopold12 <michaelleopold16@gmail.com>
2020-05-12 11:55:03 +0800

    fix scan top up merchant

--- src/services/payment_services/top_up/top_up_merchant/scanTopUpMerchant.js
+++ src/services/payment_services/top_up/top_up_merchant/scanTopUpMerchant.js
@@ -3,6 +3,8 @@ const { checkerValidMerchant } = require('../../../../collections/merchant/servi
 const { checkerValidSerial } = require('../../../../collections/serial_numbers/services')
 const { checkerValidQr, isQrExpired } = require('../../../../collections/qr/services')
 const { checkerValidUser } = require('../../../../collections/user/services')
+const { generateID, getUnixTime } = require('../../../../utils/services/supportServices')
+const { RANDOM_STRING_FOR_CONCAT } = require('../../../../utils/constants/number')
 
 // Model
 const Saldo = require('../../../../collections/saldo/Model')
@@ -36,8 +38,21 @@ const scanQrTopupMerchant = async (amount, merchantID, serialNumber, userID, tra
     // Update User saldo
     let finalSaldo = 0
     const currentSaldo = await Saldo.findOne({ user_id: userID })
-    finalSaldo = currentSaldo.saldo + amount
-    await Saldo.updateOne({ user_id: userID }, { saldo: finalSaldo })
+
+    // if already top up before
+    if (currentSaldo) {
+      finalSaldo = currentSaldo.saldo + amount
+      await Saldo.updateOne({ user_id: userID }, { saldo: finalSaldo })
+    } else {
+      await new Saldo({
+        saldo_id: generateID(RANDOM_STRING_FOR_CONCAT),
+        saldo: amount,
+        user_id: userID,
+        user_id_native: user._id,
+        created_at: getUnixTime(),
+        updated_at: getUnixTime()
+      })
+    }
 
     // Update Transaction to Settled
     await Transaction.updateOne({ transaction_id: transactionID }, { status: 'SETLD', user_id: userID, user_id_native: user._id })
